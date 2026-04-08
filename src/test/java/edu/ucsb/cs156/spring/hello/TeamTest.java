5c89b459b367b38c67fe56e5f2ac64141e955a1b
Nick Kamenica <nicholaskamenica@ucsb.edu>
2026-04-08 11:51:32 -0700

    achieve 100% mutation coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,5 +24,50 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_same_object_returns_true() {
+        Team t = new Team("test-team");
+        assertTrue(t.equals(t));
+    }
+
+    @Test
+    public void equals_different_class_returns_false() {
+        Team t = new Team("test-team");
+        assertFalse(t.equals("not a team"));
+    }
+
+    @Test
+    public void equals_same_name_same_members_returns_true() {
+        Team t1 = new Team("test-team");
+        Team t2 = new Team("test-team");
+        assertTrue(t1.equals(t2));
+    }
+
+    @Test
+    public void equals_different_name_returns_false() {
+        Team t1 = new Team("test-team");
+        Team t2 = new Team("other-team");
+        assertFalse(t1.equals(t2));
+    }
+
+    @Test
+    public void equals_same_name_different_members_returns_false() {
+        Team t1 = new Team("test-team");
+        t1.addMember("Arya");
+        Team t2 = new Team("test-team");
+        assertFalse(t1.equals(t2));
+    }
+
+    @Test
+    public void hashCode_test() {
+        Team t = new Team("test-team");
+        int result = t.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
 }
